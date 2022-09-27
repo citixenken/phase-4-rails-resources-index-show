6967c82d6f89fc859028df79c5ea5dda935a2078
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-27 10:22:53 +0300

    COMPLETED: Rails Resource Routing - Index and Show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # custom routes
+  # =============
+  # get '/birds', to: 'birds#index'
+  # get '/birds/:id', to: 'birds#show'
+
+  # using resources => RESTful routes
+  # =================================
+  resources :birds, only: [:index, :show]
 end
